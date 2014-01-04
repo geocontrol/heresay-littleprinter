8719833a0c5edbb914b99f00cd8a30cdce3a26d1
geocontrol <mark@geekyoto.com>
2014-01-04 21:22:28 +0000

    Updated the db.js model for the UserSchema.

--- heresay-littleprinter/db.js
+++ heresay-littleprinter/db.js
@@ -4,6 +4,9 @@ var UserSchema = new mongoose.Schema({
 	user_id				: String,
 	subscription_id		: String,
 	endpoint			: String,
+	borough				: String,
+	postcode			: String,
+	instance_name		: String,
 	updated_at			: Date
 });
 
